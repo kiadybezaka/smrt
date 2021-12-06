dfafc9da6ce4aca9a385786ee62da35c50c4feb2
Kiady Bezaka <kiady.razafindrabe@gmail.com>
2021-12-06 22:30:53 +0100

    [FED] Add header/container/footer

---
+++ src/components/container/Container.js
@@ -0,0 +1,11 @@
+import './Container.css';
+
+function Container() {
+  return (
+      <div className="container">
+        <p>Container</p>
+      </div>
+  );
+}
+
+export default Container;
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+      <footer className="header">
+        <p>Footer</p>
+      </footer>
+  );
+}
+
+export default Footer;
